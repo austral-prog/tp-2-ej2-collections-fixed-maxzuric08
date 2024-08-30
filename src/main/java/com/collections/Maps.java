639a7bb6df8231a80aed0f38e2001b04a2735ebb
Máximo Zúñiga <mjzunigaricardi@mail.austral.edu.ar>
2024-08-30 06:22:30 -0400

    TP2_MAXIMOZUNIGA_MAPS

--- src/main/java/com/collections/Maps.java
+++ src/main/java/com/collections/Maps.java
@@ -6,31 +6,65 @@ public class Maps {
 
     public Map<String, Integer> createInventory(List<String> items) {
        Map<String, Integer> map = new HashMap<>();
-       Set<String> set = new HashSet<>();
-       for(String element : items){ set.add(element);}
-       for(String element : set){
-       }
-               return null;
+       Set<String> set = new HashSet<>(items);
+       for(String element:set){
+           int count = 0;
+           for(String element_1 : items){
+               if(element_1.equals(element)){count+=1;}
            }
+           map.put(element,count);
        }
+               return map;
+           }
 
-    public Map<String, Integer> addItems(Map<String, Integer> inventory, List<String> items) {
-        // todo: Implement the logic to add or increment items in the inventory using elements from the items list
-        return null;
+           public Map<String, Integer> addItems(Map<String, Integer> inventory, List<String> items) {
+            Set<String> set = new HashSet<>(items);
+                for(String element:set){
+                    int count = 0;
+                    for(String element_1 : items) {
+                        if (element_1.equals(element)) {
+                            count += 1;
+                        }}
+
+                    if (!inventory.containsKey(element)){
+                    inventory.put(element,count);}
+                    else {inventory.put(element,inventory.get(element)+count);}
+
+                }
+        return inventory;
     }
 
     public Map<String, Integer> decrementItems(Map<String, Integer> inventory, List<String> items) {
-        // todo: Implement the logic to decrement items in the inventory using elements from the items list
-        return null;
+        Set<String> set = new HashSet<>(items);
+        for(String element:set){
+            int count = 0;
+            for(String element_1 : items) {
+                if (element_1.equals(element)) {
+                    count += 1;
+                }}
+
+            if (inventory.get(element)>count){
+                inventory.put(element,inventory.get(element)-count);}
+            else {inventory.put(element,0);}
+
+        }
+        return inventory;
     }
 
     public Map<String, Integer> removeItem(Map<String, Integer> inventory, String item) {
-        // todo: Implement the logic to remove an item from the inventory if it matches the item string
-        return null;
+        for(String element : inventory.keySet()) {
+        if (element==item){
+            inventory.remove(element); return inventory;}
+        }
+        return inventory;
     }
 
     public List<Map.Entry<String, Integer>> listInventory(Map<String, Integer> inventory) {
-        // todo: Implement the logic to create a list containing all (item_name, item_count) pairs in the inventory
-        return null;
+        for(String element : inventory.keySet()) {
+            if (inventory.get(element)<=0){
+                inventory.remove(element);
+        }
     }
-}
+        List<Map.Entry<String, Integer>> new_list= new ArrayList<>(inventory.entrySet());
+        return new_list;
+}}
